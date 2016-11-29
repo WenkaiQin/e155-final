b3c3ecd1ebde79683c1a5c84dc363f670e597742
Wenkai Qin <wqin@hmc.edu>
2016-11-29 15:13:21 -0800

    simulation confirmed working for v2

--- uart_receiver_v2.sv
+++ uart_receiver_v2.sv
@@ -1,8 +1,8 @@
 module testbench();
-	logic clk, reset, rx;
+	logic clk, creset, reset, rx;
 	logic [7:0] data;
 	
-	finalProject fp0(clk, reset, rx, data);
+	final_project fp0(clk, creset, reset, rx, data);
 	
 	initial
 		forever begin
@@ -12,10 +12,12 @@ module testbench();
 		
 	initial begin
 		rx = 1'b1;
-		reset = 1'b1; #20; reset = 1'b0;
+		reset = 1'b1; creset = 1'b1;
+		#10; creset = 1'b0;
+		#10; reset = 1'b0;
 		#100;
-		rx = 1'b0; #64;
 		
+		rx = 1'b0; #64;
 		rx = 1'b1; #64;
 		rx = 1'b0; #64;
 		rx = 1'b1; #64;
@@ -29,23 +31,25 @@ module testbench();
 endmodule
 
 module final_project(input  logic clk,
-						  input  logic reset,
-						  input  logic rx,
-						  output logic [7:0] data);
+							input  logic creset,
+							input  logic reset,
+							input  logic rx,
+							output logic [7:0] data);
 	
-	uart_rx dut(clk, reset, rx, data, test);
+	uart_rx dut(clk, creset, reset, rx, data, test);
 	
 endmodule
 
 module uart_rx(input  logic clk,
+					input  logic creset,
 					input  logic reset,
                input  logic rx,
 					output logic [7:0] data,
 					output logic test);
 	
 	logic bck, sck, valid;
-	clk_gen cg0(clk, reset, bck, sck);
-	rx_state fsm0(bck, reset, rx, data);
+	clk_gen cg0(clk, creset, bck, sck);
+	rx_state fsm0(sck, reset, rx, data);
 			
 endmodule
 
@@ -72,11 +76,12 @@ module rx_state(input  logic clk,
 		if(reset) begin
 			state <= S0;
 			count <= 8'b0;
-			bit_c <= 3'b0;
 			tdata <= 8'b0;
 			data <= 8'b0;
 
 		end else begin
+			state <= nextstate;
+			
 			if(read_en)
 				tdata <= {rx, tdata[7:1]};
 			    // tdata[bit_c] <= rx;
@@ -93,7 +98,7 @@ module rx_state(input  logic clk,
 		
 	// Combination logic for state transition
 	always_comb begin
-		case(state)
+		case(state) 	
 			// If rx is low, go to next state
 			S0: if(!rx) nextstate = S1;
 				else	nextstate = S0;
@@ -109,14 +114,12 @@ module rx_state(input  logic clk,
 			default: nextstate = S0;
 
 		endcase
-
-		read_en   = ((count-7) % 16 == 16);
-		count_en  = (state != S0);
-		out_en    = (state == S3);
-
-	end
-
-
+	end 
+	
+	assign read_en   = ((count-7) % 16 == 0);
+	assign count_en  = (state != S0);
+	assign out_en    = (state == S3);
+	assign bit_c	  = (count-8) / 16;
 
 endmodule
 
@@ -128,7 +131,7 @@ module clk_gen(input  logic clk,
 
 	logic [17:0] counter;
 	logic [17:0] increment = 18'b111111;
-	always_ff@(posedge clk, posedge reset)	
+	always_ff@(posedge clk)	
 		if(reset) counter <= 18'b0;
 		else counter <= counter + increment;
 //	assign bck = counter[17];
